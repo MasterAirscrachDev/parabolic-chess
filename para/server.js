5fcc82d6be88314c43750c556a1320942df312f4
boo <81114382+MellowYellow7777@users.noreply.github.com>
2026-03-24 18:03:31 -0500

    Update server.js

--- para/server.js
+++ para/server.js
@@ -180,5 +180,7 @@ wss.on('connection', ws => {
   });
 });
 
+app.use(express.static(path.join(__dirname, "public")));
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => console.log(`serving on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Serving on port ${PORT}`));
