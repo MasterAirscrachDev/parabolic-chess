bee82d13f2b8e71c8bc67f9c46aa654d2915f81d
boo <81114382+MellowYellow7777@users.noreply.github.com>
2026-03-24 17:57:55 -0500

    Update net.js

--- para/public/net.js
+++ para/public/net.js
@@ -58,7 +58,7 @@ border-radius:8px;border:none;width:140px">`;
 
   function connect() {
     var proto = location.protocol === 'https:' ? 'wss' : 'ws';
-    ws = new WebSocket('parabolic-chess-production.up.railway.app');
+    ws = new WebSocket('wss://parabolic-chess-production.up.railway.app');
 
     ws.onopen = () => showLobby();
 
